e1453f24bfecfc06f23b98ee867c1956bca76825
obzva <dngyng1000@gmail.com>
2023-09-17 23:00:37 +0900

    chapter3 section3.2 reading

---
+++ chapter3/section3.2/scanf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int i, j;
+  float x, y;
+
+  scanf("%d%d%f%f", &i, &j, &x, &y);
+
+  printf("i: %d, j: %d, x: %f, y: %f\n", i, j, x, y);
+
+  return 0;
+}
