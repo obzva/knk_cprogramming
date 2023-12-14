2b1400c811473aedbd0667220c22de0c21dc755d
obzva <dngyng1000@gmail.com>
2023-12-15 01:40:02 +0900

    chapter6 section6.1

---
+++ chapter6/section6.1/compound-statement.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int i = 10;
+  while (i > 0) {
+    printf("T minus %d and counting\n", i);
+    i--;
+  }
+  return 0;
+}
---
+++ chapter6/section6.1/smallest-power-of-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  int i = 1, n = 10;
+  while (i < n)
+    i = i * 2;
+  printf("%d\n", i);
+  return 0;
+}
